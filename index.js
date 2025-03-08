b4c1583438f36b45ea218712a2d7c55e498ca9a4
“rvaneek” <dubeyamar660@gmail.com>
2025-03-08 15:36:13 +0530

    Hello

---
+++ index.js
@@ -0,0 +1,30 @@
+// const cords = {
+//     x: 0,
+//     y: 0,
+// }
+//
+// const circles = document.querySelector('.circle');
+//
+//
+// circles.x = 0
+// circles.y = 0
+//
+//
+// window.addEventListener('mousemove', (e) => {
+//     cords.x = e.clientX;
+//     cords.y = e.clientY;
+//
+//     animate()
+// })
+//
+// function animate() {
+//     let x = cords.x
+//     let y = cords.y
+//
+//     // console.log(cords)
+//
+//
+//     circles.style.left = x - 12 + "px";
+//     circles.style.top = y - 12 + "px";
+//
+// }
